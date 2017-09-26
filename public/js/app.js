43cbbf332fe7233b8887ea79ccf1c380dda2ef68
Kristin Takehara <KRISTINTAKEHARA@Kristins-MacBook-Pro.local>
2017-09-26 11:29:54 -1000

    including teacher notations. need to refactor code to be modular

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 console.log('here we go again');
 
-//callBack function for XMLH
-function makeReq(callBack, URL) {
+//make XHR request
+function makeReq(URL, callBack) {
   let newReq = new XMLHttpRequest();
   newReq.addEventListener("load", callBack);
   newReq.open("GET", URL);
@@ -19,4 +19,30 @@ function requestElement(htmlElement) {
 function assignElement (elementName, className, appendTo){
   elementName.class = className;
   appendTo.appendChild(elementName);
+}
+
+function buildSubreddit(subreddit) {
+  //variables
+  let parent = document.getElementById('root');
+
+  //get subreddit data
+  let query = { url: `http://www.reddit.com/r/${subreddit}.json`};
+  request(query, function() {
+    let response = JSON.parse(this.responseText);
+    let posts = response.data.children;
+
+    //replace with children
+    parent.innerHTML = '';
+    posts.forEach((post)) => {
+      // let postElement = buildPost(post);
+      parent.appendChild(postElement);
+      console.log(post.data);
+      }
+  };
+      function buildPost(post){
+        console.log(posts);
+  }
+
+
+
 }
