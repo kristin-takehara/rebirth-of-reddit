507371aac163236d71199c37b3e779ad8c417b1a
Kristin Takehara <KRISTINTAKEHARA@Kristins-MacBook-Pro.local>
2017-09-25 22:42:29 -1000

    header and nav menu up

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,22 @@
 console.log('here we go again');
 
+//callBack function for XMLH
 function makeReq(callBack, URL) {
-  var newReq = new XMLHttpRequest();
+  let newReq = new XMLHttpRequest();
   newReq.addEventListener("load", callBack);
   newReq.open("GET", URL);
   newReq.send();
 }
 
+//request Element from the data
+function requestElement(htmlElement) {
+  return function() {
+    let data = JSON.parse(this.responseText);
+  };
+}
+
+//append the dynamically retrieved element to the proper html element.
+function assignElement (elementName, className, appendTo){
+  elementName.class = className;
+  appendTo.appendChild(elementName);
+}
